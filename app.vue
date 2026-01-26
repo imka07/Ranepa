af1b79a0a236dd236b8c063d19e541233a49af16
Shokuev <93195583+imka07@users.noreply.github.com>
2026-01-26 19:09:15 +0300

    added vercel analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="bg-black min-h-screen">
     <NuxtPage />
+    <Analytics />
   </div>
 </template>
+
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
